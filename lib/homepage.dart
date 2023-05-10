b7e856ba7553e966fd6b3893a332f3357749a4c1
tarik benaouda <ta.benaouda@esi-sba.dz>
2023-05-11 00:11:07 +0100

    tarik String

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -19,7 +19,7 @@ class _HomePageState extends State<HomePage> {
         // ignore: prefer_const_literals_to_create_immutables
         children: [
           const Text(
-            "Hello, you're logged in §§fvndudbd ",
+            "Hello, you're logged in §§fvndudbd tarik ",
             style: TextStyle(fontSize: 25),
           ),
           SizedBox(height: 10),
