3bdc99698fb6bf302bea456d6bc72d1a1351093c
HossemCharrak <h.charrak@esi-sba.dz>
2023-05-14 19:25:13 +0200

    Logout backend

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomeView(),
+      home: const SplashScreen(),
       routes: {
         //  "/": (context) => Auth(),
         "welcome": (context) => const Unboarding(),
