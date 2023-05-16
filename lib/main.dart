61da8b24340f494aabe20c1f09ec2dd94bcf5b05
hako <hakodjerad@gmail.com>
2023-05-16 18:06:08 +0100

    Hako

--- lib/main.dart
+++ lib/main.dart
@@ -36,10 +36,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-
-      home: const DemandForCoaching(),
-      //  home: const SplashScreen(),
-
+      home: const SplashScreen(),
       routes: {
         //  "/": (context) => Auth(),
         "welcome": (context) => const Unboarding(),
